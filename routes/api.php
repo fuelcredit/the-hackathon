add302ef1b32edfa0a47ad9f86cd328f45e21c2e
Osuntola Fiyinfoluwa <osuntee@gmail.com>
2022-08-07 08:21:56 +0100

    routes

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,11 @@ use App\Http\Controllers\API\AuthController;
 */
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout']);
+//Route::post('/logout', [AuthController::class, 'logout']);
+
+Route::group(['middleware' => ['auth:sanctum']], function() {    
+    Route::post('/logout',   [AuthController::class, 'logout']);
+  });
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();

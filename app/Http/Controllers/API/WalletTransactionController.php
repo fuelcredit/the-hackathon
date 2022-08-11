922ab19edec10e7627ffcf33962e45cb4a3cbc1e
fuelcredit <repo@myfuelcredit.com>
2022-08-11 23:24:55 +0100

    Updated wallet balance

--- app/Http/Controllers/API/WalletTransactionController.php
+++ app/Http/Controllers/API/WalletTransactionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Wallet;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -42,6 +43,48 @@ class WalletTransactionController extends Controller
             'headers' => $headers,
             'verify'  => false,
         ]);
+        $total_amount = $data['amount'];
+
+        Wallet::where(['userId'=>$user_id])->update(['wallet_balance'=>$total_amount]);
 
     }
+
+    public function fundWalletFromMerchant(){
+        $user_id = Auth::user()->id;
+
+        $data = $request->validate([
+            'amount'=> 'required',
+            'narration'=> 'required',
+            'reference'=> 'required',
+            'product_code'=> 'required',
+            'channel_code'=> 'required',
+        ],
+        [
+            'amount.required'=> 'Amount is required',
+        ]);
+        $client = new Client();
+        $params = [
+                "amount" => $data['amount'],
+                "narration" => $data['narration'],
+                "reference" => $data['reference'],
+                "channel_code" => $data['channel_code'],
+                "product_code" => $data['product_code'],
+        ];
+
+        $headers = [
+            'ClientId' => env('ENAIRA_CLIENT_ID'),
+            'Cache-Control' => 'no-cache'
+        ];
+
+        $url = env('ENAIRA_URL').'/CreateInvoice';
+
+        $response = $client->request('POST', $url, [
+            'json' => $params,
+            'headers' => $headers,
+            'verify'  => false,
+        ]);
+
+    }
+
 }
+
